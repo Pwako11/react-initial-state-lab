09f6a3be35750d9b9dd552d8cf6b20fd22ff35e8
Pwako11 <akolo@hotmail.com>
2021-06-29 13:56:06 -0400

    commit tree 8b2ced40e2e70ded82270eb6d461b281c8069d64

--- src/Bomb.js
+++ src/Bomb.js
@@ -1,1 +1,32 @@
 // your Bomb code here!
+import React, { Component } from 'react';
+// import ImageSlider from './ImageSlider.js';
+
+export default class Bomb extends Component {
+
+    constructor(props) {
+        super()
+        this.state = {
+            secondsLeft: props.initialCount
+        }
+      }
+
+   
+
+    render(){
+        const time = this.state.secondsLeft;
+        let countDown;
+        if (time === 0){
+           countDown = <p>Boom!</p>
+        } else {
+            countDown = <p>{time} seconds left before I go boom!</p>
+        };
+        return (
+
+            <div ClassName="bomb"> 
+                {countDown}
+            </div>
+        )
+    }
+
+}
